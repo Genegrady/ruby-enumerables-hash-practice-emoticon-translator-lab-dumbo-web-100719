df019eeb22ca1ed0532c58bbaa0bcb7a6a6dd61b
Eugene Grady <grady.gene.thomas@gmail.com>
2019-09-30 21:20:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require "yaml"
 
 
 def load_library(file)
- library = YAML.load(".lib/emoticons.yml")
+ library = YAML.load_file(".lib/emoticons.yml")
  emoticons_hash = {
    "get_meaning" => {},
    "get_emoticon" => {}
