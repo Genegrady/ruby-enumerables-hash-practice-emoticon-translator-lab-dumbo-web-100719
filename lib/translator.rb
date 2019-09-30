90617fe81141039bec7f89c2f3cb210eeda9b920
Eugene Grady <grady.gene.thomas@gmail.com>
2019-09-30 21:17:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,15 +3,15 @@ require "yaml"
 
 
 def load_library(file)
- emoticons = YAML.load(".lib/emoticons.yml")
+ library = YAML.load(".lib/emoticons.yml")
  emoticons_hash = {
    "get_meaning" => {
      
    },
    "get_emoticon" => {}
  }
- emoticons.each do |meaning, emoticons|
-    remoticons_hash["get_meaning"][emoticons[1]] = meaning
+ library.each do |meaning, emoticons|
+    emoticons_hash["get_meaning"][emoticons[1]] = meaning
     emoticons_hash["get_emoticon"][emoticons[0]] = emoticons[1]
   end
   result
